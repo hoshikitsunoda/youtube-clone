b950238dad44ca689f14d48e7ca166a3034dfe70
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-05 17:33:14 -0700

    setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const App = () => {
-  return <div>YouTube Clone</div>
+const App: React.FC = () => {
+  return (
+    <div>
+      <input
+        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        type="email"
+        placeholder="jane@example.com"
+      />
+    </div>
+  )
 }
 
 export default App
